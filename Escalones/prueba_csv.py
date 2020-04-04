6a5cb9a6a274b082a8686de09cd9eb12518e8fd1
jeisongarcia9713 <jegarciat@correo.udistrital.edu.co>
2020-04-04 17:38:37 -0500

    nuevos datos con lazo cerrado para caracterizar

--- Escalones/prueba_csv.py
+++ Escalones/prueba_csv.py
@@ -4,9 +4,9 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
-datos=pd.read_csv('datos_3_suelo_adelante_1060.txt',header=0)
+datos=pd.read_csv('datos_6_suelo_adelante_1060.txt',header=0)
 tiempo=datos['t']
-entrada=datos['i4']
+entrada=datos['i3']
 
 fig, ax = plt.subplots()
 ax.plot(tiempo, entrada)
--- Escalones/serialp.py
+++ Escalones/serialp.py
@@ -29,7 +29,7 @@ def guardar():
         Error = pd.Series(Error1,name='i6')
 
         m =pd.concat([Tiempo,In,Mot1,Mot2,Mot3,Mot4,Error],axis=1)
-        m.to_csv('datos_1_suelo_adelante_1060.txt',header=True,index=False)
+        m.to_csv('datos_6_suelo_adelante_1060.txt',header=True,index=False)
         print('termino')
 
 if __name__ == "__main__":
